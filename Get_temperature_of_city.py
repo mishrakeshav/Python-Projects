090cf8b1230a45f1907f7df9cb01eacc782fb598
Keshav Mishra <47028747+mishrakeshav@users.noreply.github.com>
2019-10-12 17:29:54 +0530

    Web Scraping in python

---
+++ Get_temperature_of_city.py
@@ -0,0 +1,22 @@
+import socket
+import requests
+try:
+	city = input("Enter city name: ")
+	socket.create_connection(("www.google.com", 80))
+	a1 = "http://api.openweathermap.org/data/2.5/weather?units=metric"
+	a2 = "&q=" + city
+	a3 = "&appid=c6e315d09197cec231495138183954bd"
+	
+	api_address = a1 + a2 + a3
+	res1 = requests.get(api_address)
+	print(res1)
+	
+	j1 = res1.json()
+	print(j1)
+
+	d1 = j1['main']
+	print(d1)
+	temp = d1['temp']
+	print("Temp : ", temp)
+except OSError:
+	print("Check network")
